c633584806fb83285c03854472a0780c4a9c303c
Gabriel-Sanchez <gabriel.g.sanchez.c@gmail.com>
2024-02-26 21:37:06 -0500

    datos habitos

--- AppHabitosWeb/habitos_app/admin.py
+++ AppHabitosWeb/habitos_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Habito, TiposHabitos, Historial_habitos
 
 # Register your models here.
+
+admin.site.register(Habito)
+admin.site.register(TiposHabitos)
+admin.site.register(Historial_habitos)
--- AppHabitosWeb/habitos_app/models.py
+++ AppHabitosWeb/habitos_app/models.py
@@ -8,6 +8,10 @@ from django.db import models
 class TiposHabitos(models.Model):
     nombre = models.CharField(max_length=100)
     numero = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
+    
 
 class Habito(models.Model):
     numero = models.IntegerField()
@@ -20,6 +24,11 @@ class Habito(models.Model):
     color = models.CharField(max_length=7,default='FFFFFF')
     objetivo = models.IntegerField(default=0)
     progresion = models.IntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.nombre + '-' + str(self.numero) 
+    
     
 class Historial_habitos(models.Model):
     fk_habito = models.ForeignKey(Habito, on_delete=models.CASCADE)
