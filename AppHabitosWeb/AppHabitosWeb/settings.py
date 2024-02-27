733070c1bf1d978c15cbbb4a17206d6d92519b34
Gabriel-Sanchez <gabriel.g.sanchez.c@gmail.com>
2024-02-26 20:34:58 -0500

    app model

--- AppHabitosWeb/AppHabitosWeb/settings.py
+++ AppHabitosWeb/AppHabitosWeb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base_app'
+    'base_app',
+    'habitos_app'
 ]
 
 MIDDLEWARE = [
--- AppHabitosWeb/base_app/models.py
+++ AppHabitosWeb/base_app/models.py
@@ -1,27 +1,2 @@
 from django.db import models
 
-# Create your models here.
-
-
-class TiposHabitos(models.Model):
-    nombre = models.CharField(max_length=100)
-    numero = models.IntegerField()
-
-class Habito(models.Model):
-    numero = models.IntegerField()
-    nombre = models.CharField(max_lenght=150)
-    work_time = models.IntegerField()
-    short_break = models.IntegerField()
-    count = models.IntegerField()
-    type = models.ForeignKey(TiposHabitos, on_delete=models.CASCADE)
-    orden_n = models.IntegerField()
-    color = models.CharField(max_length=7,default='FFFFFF')
-    objetivo = models.IntegerField(default=0)
-    progresion = models.IntegerField(default=0)
-    
-class Historial_habitos(models.Model):
-    fk_habito = models.ForeignKey(Habito, on_delete=models.CASCADE)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
-    duracion = models.DurationField()
-    duracion_descanso = models.DurationField()
